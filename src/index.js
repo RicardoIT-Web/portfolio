fbaad11d94c62ad7ce93b75df51f5fcda7364c46
RicardoIT-Web <rick_piedade@hotmail.com>
2022-09-16 14:49:38 +0100

    add media queries for small screens and tablets for scroll and social features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// ReactDOM.render(<App/>, document.querySelector('#root'));
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import './index.css';
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
