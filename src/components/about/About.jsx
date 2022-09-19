4d9bd0150c963054b5c58c112e7d7f2d85b7f933
RicardoIT-Web <rick_piedade@hotmail.com>
2022-09-19 19:09:20 +0100

    Add about content and styling to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me5.png'
+import {GiDiploma} from 'react-icons/gi'
+import {BsPeopleFill} from 'react-icons/bs'
+import {FaProjectDiagram} from 'react-icons/fa'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-img">
+            <img src={ME} alt="About me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <GiDiploma className="about__icon" />
+              <h5>Experience</h5>
+              <small>10 + Years Experience</small>
+            </article>
+            <article className="about__card">
+              <BsPeopleFill className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+</small>
+            </article>
+            <article className="about__card">
+              <FaProjectDiagram className="about__icon" />
+              <h5>Projects</h5>
+              <small>All Projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio a
+            rerum pariatur cupiditate exercitationem nemo dolorum repellendus.
+            Accusamus neque earum sint quos quibusdam fuga vero inventore cum
+            eligendi quidem. Ab.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About
