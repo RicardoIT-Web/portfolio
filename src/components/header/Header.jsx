49c1552b24cf40a3e16d8e175468bdb801de0b87
RicardoIT-Web <rick_piedade@hotmail.com>
2022-09-21 20:53:20 +0100

    Adjust images on portfolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CTA from './CTA'
-import ME from '../../assets/me.png'
+import ME from '../../assets/me9.png'
 import HeaderSocials from './HeaderSocials'
 import './header.css'
 
