fe95a3b0c16a65c1a12637d502997b024d829cd0
RicardoIT-Web <rick_piedade@hotmail.com>
2022-09-22 15:10:57 +0100

    install swiper and add swiper styling to testimonials section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,72 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/c-soars.png'
+import AVTR2 from '../../assets/diners3starrestaurant.png'
+import AVTR3 from '../../assets/motorbikebuddy.png'
+
+// import Swiper core and required modules
+import {Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "C-Soars Ltd",
+    review:
+      "What an amazing website. Loving the images and colour schemes. The site is now ready to go and start taking bookings and payments.",
+  },
+  {
+    avatar: AVTR2,
+    name: "diners 3star",
+    review:
+      "Fantastic work produced by Ricardo. Work was methodical and well planned. Ricardo produced exactly what we needed on time and on budget.",
+  },
+  {
+    avatar: AVTR3,
+    name: "MotorBikeBuddy",
+    review:
+      "Brilliant UI/UX works here. The colouring schemes are fantastic. I can't wait to see the final result.",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <div>testimonials</div>
-  )
+    <section id="testimonials">
+      <h5>Client Feedbacks</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+            return (
+                <SwiperSlide key={index} className="testimonial">
+                    <div className="client__avatar">
+                        <img src={avatar} alt="Drone Survey Company feedback" />
+                    </div>
+                    <h5 className="client__name">{name}</h5>
+                    <small className="client__review">
+                        {review}
+                    </small>
+                </SwiperSlide>
+                )
+            })
+        }
+      </Swiper>
+    </section>
+  );
 }
 
 export default Testimonials
