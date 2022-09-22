160ca8e18bf22169594dd24b86e36f21a61a39b3
RicardoIT-Web <rick_piedade@hotmail.com>
2022-09-22 21:30:47 +0100

    initiate footer content and styling section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {AiFillInstagram} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer id='footer'>
+      <a href="#header" className='footer__logo'>Ricardo</a>
+
+      <ul className='links'>
+        <li><a href="#header">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contacts">Contacts</a></li>
+      </ul>
+
+      <div className="footer__socials">
+      <a href="https://github.com/RicardoIT-Web" target="_blank" rel='noreferrer'><FaGithub /></a>
+      <a href="https://www.linkedin.com/in/rpiedade/" target="_blank" rel='noreferrer'><BsLinkedin /></a>
+      <a href="https://www.instagram.com/rick.piedade/" target="_blank" rel='noreferrer'><AiFillInstagram /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; RicardoPiedade</small>
+      </div>
+    </footer>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,8 +6,8 @@ import {AiFillInstagram} from 'react-icons/ai'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/rpiedade/" target="_blank" rel='noreferrer'><BsLinkedin /></a>
         <a href="https://github.com/RicardoIT-Web" target="_blank" rel='noreferrer'><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/rpiedade/" target="_blank" rel='noreferrer'><BsLinkedin /></a>
         <a href="https://www.instagram.com/rick.piedade/" target="_blank" rel='noreferrer'><AiFillInstagram /></a>
     </div>
   )
